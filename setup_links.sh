9b41259728c09f713f8bb5c4a838cc74951bcfa3
Simon Berger <simberger@gmail.com>
2013-03-03 18:42:56 +0100

    added link for arch

--- setup_links.sh
+++ setup_links.sh
@@ -1,7 +1,9 @@
 BUILD_DIR=`readlink -f $1`
-
+ARCH_DIR=`readlink -f $2`
 
 
 ln -s $BUILD_DIR/dd dd
 ln -s $BUILD_DIR/dd1_cl/libcl.so dd1/cl.so.linux.i386
 ln -s $BUILD_DIR/dd1_sv/libsv.so dd1/sv.so.linux.i386
+
+ln -s $ARCH_DIR dd1/arch00.dir
